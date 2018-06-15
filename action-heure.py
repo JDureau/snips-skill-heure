beecbd04d20b7482e105087968f762395b820e8e
Joseph Dureau <joseph.dureau@snips.ai>
2018-06-15 20:10:41 +0200

    verbalise hour

--- action-heure.py
+++ action-heure.py
@@ -7,32 +7,34 @@ MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
-verbalisation = {
-	"0": "minuit",
-	"1": "une heure",
-	"2": "deux heures",
-	"3": "trois heures",
-	"4": "quatre heures",
-	"5": "cinq heures",
-	"6": "six heures",
-	"7": "sept heures",
-	"8": "huit heures",
-	"9": "neuf heures",
-	"10": "dix heures",
-	"11": "onze heures",
-	"12": "midi",
-	"13": "treize heures",
-	"14": "quatorze heures",
-	"15": "quinze heures",
-	"16": "seize heures",
-	"17": "dix-sept heures",
-	"18": "dix-huit heures",
-	"19": "dix-neuf heures",
-	"20": "ving heures",
-	"21": "ving-et-une heures",
-	"22": "vingt deux heures",
-	"23": "vingt troi heures"
-}
+def verbalise_hour(i):
+	if i == 0:
+		return "minuit"
+	elif i == 1:
+		return "une heure"
+	elif i == 12:
+		return "midi"
+	elif i == 21:
+		return "ving et une heures"
+	else:
+		return "{0} heures".format(str(i)) 
+
+def verbalise_minute(i):
+	if i == 0:
+		return ""
+	elif i == 1:
+		return "une"
+	elif i == 21:
+		return "ving et une"
+	elif i == 31:
+		return "trente et une"
+	elif i == 41:
+		return "quarante et une"
+	elif i == 51:
+		return "cinquante et une"
+	else:
+		return "{0} heures".format(str(i)) 
+
 
 def intent_received(hermes, intent_message):
     sentence = 'Il est '
@@ -42,7 +44,7 @@ def intent_received(hermes, intent_message):
 
     now = datetime.now()
 
-    sentence += str(now.hour) + " heures " + str(now.minute)
+    sentence += verbalise_hour(now.hour) + verbalise_minute(now.minute)
     print(sentence)
 
     hermes.publish_end_session(intent_message.session_id, sentence)
