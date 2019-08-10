719f0f8d383b7f7db633065a33dc55db6385e539
dohseven <tho_jea+framagit@protonmail.com>
2019-08-10 16:01:26 +0200

    Code clean-up: new lines at end of file and spaces instead of tabs

--- action-heure.py
+++ action-heure.py
@@ -9,57 +9,56 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 def verbalise_hour(i):
-	if i == 0:
-		return "minuit"
-	elif i == 1:
-		return "une heure"
-	elif i == 12:
-		return "midi"
-	elif i == 21:
-		return "vingt et une heures"
-	else:
-		return "{0} heures".format(str(i)) 
+    if i == 0:
+        return "minuit"
+    elif i == 1:
+        return "une heure"
+    elif i == 12:
+        return "midi"
+    elif i == 21:
+        return "vingt et une heures"
+    else:
+        return "{0} heures".format(str(i))
 
 def verbalise_minute(i):
-	if i == 0:
-		return ""
-	elif i == 1:
-		return "une"
-	elif i == 21:
-		return "vingt et une"
-	elif i == 31:
-		return "trente et une"
-	elif i == 41:
-		return "quarante et une"
-	elif i == 51:
-		return "cinquante et une"
-	else:
-		return "{0}".format(str(i)) 
+    if i == 0:
+        return ""
+    elif i == 1:
+        return "une"
+    elif i == 21:
+        return "vingt et une"
+    elif i == 31:
+        return "trente et une"
+    elif i == 41:
+        return "quarante et une"
+    elif i == 51:
+        return "cinquante et une"
+    else:
+        return "{0}".format(str(i))
 
 
 def intent_received(hermes, intent_message):
+    print()
+    print(intent_message.intent.intent_name)
+    print()
 
-	print()
-	print(intent_message.intent.intent_name)
-	print ()
+    if intent_message.intent.intent_name == 'Joseph:askTime':
 
-	if intent_message.intent.intent_name == 'Joseph:askTime':
+        sentence = 'Il est '
+        print(intent_message.intent.intent_name)
 
-		sentence = 'Il est '
-		print(intent_message.intent.intent_name)
+        now = datetime.now(timezone('Europe/Paris'))
 
-		now = datetime.now(timezone('Europe/Paris'))
+        sentence += verbalise_hour(now.hour) + " " + verbalise_minute(now.minute)
+        print(sentence)
 
-		sentence += verbalise_hour(now.hour) + " " + verbalise_minute(now.minute)
-		print(sentence)
+        # hermes.publish_continue_session(intent_message.session_id, sentence, ["Joseph:greetings"])
+        hermes.publish_end_session(intent_message.session_id, sentence)
 
-		# hermes.publish_continue_session(intent_message.session_id, sentence, ["Joseph:greetings"])
-		hermes.publish_end_session(intent_message.session_id, sentence)
+    elif intent_message.intent.intent_name == 'Joseph:greetings':
 
-	elif intent_message.intent.intent_name == 'Joseph:greetings':
-
-		hermes.publish_end_session(intent_message.session_id, "De rien!")
+        hermes.publish_end_session(intent_message.session_id, "De rien!")
 
 
 with Hermes(MQTT_ADDR) as h:
-	h.subscribe_intents(intent_received).start()
+    h.subscribe_intents(intent_received).start()
